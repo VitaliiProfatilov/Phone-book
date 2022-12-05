470fb2d581331dc5399502243672f50adfe9f604
VitaliiProfatilov <vitaliiproftl@gmail.com>
2022-12-05 13:40:55 +0200

    first commit

---
+++ src/components/PhoneBook/PhoneBook.js
@@ -0,0 +1,12 @@
+import styles from './PhoneBook.module.scss';
+import React from 'react';
+
+export function PhoneBook() {
+
+
+    return (
+        <div className={styles.container}>
+            1111
+        </div>
+    )
+}
